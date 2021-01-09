c9d495f9462e7f50dc2d44d85870735823e62f5a
pawel.marcinowski11 <pawel.marcinowski11@gmail.com>
2021-01-09 22:05:12 +0100

    Poszerzenie zakresu obsługiwanych liczb

--- Zadanie_2_Marcinowski/Zadanie_2_Marcinowski/Zadanie_2_Marcinowski.cpp
+++ Zadanie_2_Marcinowski/Zadanie_2_Marcinowski/Zadanie_2_Marcinowski.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <math.h>
 #include <string>
+#include <sstream>
 
 typedef enum { bin, dec };
 
@@ -8,10 +9,12 @@ typedef enum { bin, dec };
 * Funkcja zamienia podaną wartość binarną na dziesiętną
 *
 */
-long toDec(std::string bin)
+unsigned long long toDec(std::string bin)
 {
+	//return std::stoull(bin, nullptr, 2); //Też działa, ale to by było za proste
+
 	std::string cutNumber = bin;
-	long result = 0;
+	unsigned long long result = 0;
 	int power = pow(2, cutNumber.size() - 1); //Dodajemy cyfry od lewej do prawej, więc zaczynamy od najwyższej potęgi liczby 2
 	for (int i = 0; i < cutNumber.size(); i++, power /= 2) {
 		if (cutNumber[i] == '1') {
@@ -26,14 +29,14 @@ long toDec(std::string bin)
 * Funkcja zamienia podaną wartość dziesiętną na binarną
 *
 */
-std::string toBin(long dec)
+std::string toBin(unsigned long long dec)
 {
-	if (dec == 0) //Opisuję "0" jako szczególny przypadek, gdyż log(0) jest niepoprawną operacją
+	if (dec == 0)               //Opisuję "0" jako szczególny przypadek, gdyż log(0) jest niepoprawną operacją
 		return "0";
-	int power = log2(dec); //Korzystam z logarytmu, aby znaleźć najwyższą potęgę liczby 2, która zmieści się w podanej wartości
+	int power = log2(dec);      //Korzystam z logarytmu, aby znaleźć najwyższą potęgę liczby 2, która zmieści się w podanej wartości
 
 	std::string result;
-	for (long reference = pow(2, power); reference > 0; reference /= 2) {
+	for (unsigned long long reference = pow(2, power); reference > 0; reference /= 2) {
 		if (dec >= reference) { //Jeżeli w konwertowanej liczbie dziesiętnej zmieści się dana potęga liczby 2,
 								//odejmujemy ją i dodajemy do wynikowej liczby binarnej
 			result += "1";
@@ -53,55 +56,52 @@ int main()
 	std::string input;
 
 	while (1) {
-		if (mode == bin) {
-			std::cout << "Tryb dzialania: bin --> dec\n";
-		}
-		else {
+		if (mode == bin)									//Wyświetlamy użytkownikowi informacje o trybie dzialania programu,
+			std::cout << "Tryb dzialania: bin --> dec\n";   //a także instrukcję jego użytkowania
+		else
 			std::cout << "Tryb dzialania: dec --> bin\n";
-		}
+
 		std::cout << "Aby zmienic tryb dzialania, wpisz \"zmiana\"\n";
 		std::cout << "Aby opuscic program, wpisz \"wyjscie\"\n\n";
 
-		if (mode == bin) {
+		if (mode == bin)
 			std::cout << "Podaj dodatnia liczbe binarna: ";
-		}
-		else {
+		else
 			std::cout << "Podaj dodatnia liczbe dziesietna: ";
-		}
 
 		std::cin >> input;
 
-		if (input == "wyjscie")
+		if (input == "wyjscie") //Jeżeli użytkownik chce zamknąć program, przerywamy pętlę nieskończoną.
 			break;
 		else if (input == "zmiana")
-			mode == bin ? mode = dec : mode = bin;
+			mode == bin ? mode = dec : mode = bin; //Jeżeli tryb jest binarny, zostanie zmieniony na dziesiętny - i odwrotnie.
 		else {
-			char* p;
-			long long val = strtol(input.c_str(), &p, 0); //Funkcja zwraca wartość typu int, więc wynik będzie nie większy niż 2147483647
+			try {
+				unsigned long long val = std::stoull(input); //Funkcja zwraca wartość typu unsigned long long, więc nie obsłuży wartości wyższych niż 18446744073709551615
 
-			std::cout << std::endl;
+				std::cout << std::endl;
 
-			if (mode == bin) {
-				if (input.find_first_not_of("01") != std::string::npos)
-					std::cout << "Podana wartosc nie jest prawidlowa!" << std::endl;
-				else
-					std::cout << "Wynik dziesietny: " << toDec(input) << std::endl;
+				if (mode == bin) {
+					if (input.find_first_not_of("01") != std::string::npos) //Upewniamy się, że string repezentujący binarną liczbę, nie zawiera innych znaków niż 0 i 1
+						std::cout << "Podana wartosc nie jest prawidlowa!" << std::endl;
+					else
+						std::cout << "Wynik dziesietny: " << toDec(input) << std::endl;
+				}
+				else {
+					if (input.find_first_not_of("0123456789") != std::string::npos)
+						std::cout << "Podana wartosc nie jest prawidlowa!" << std::endl;
+					else
+						std::cout << "Wynik binarny: " << toBin(val) << std::endl;
+				}
 			}
-			else {
-				if (input.find_first_not_of("0123456789") != std::string::npos)
-					std::cout << "Podana wartosc nie jest prawidlowa!" << std::endl;
-				else if (val >= 2147483647)
-					std::cout << "Podana wartosc jest zbyt wielka!" << std::endl;
-				else
-					std::cout << "Wynik binarny: " << toBin(val) << std::endl;
+			catch (std::exception e) { //Łapiemy wyjątki, które może wyrzucić funkcja std::stoull
+				std::cout << "Podana wartosc nie jest prawidlowa!" << std::endl;
 			}
-
 		}
 		std::cout << std::endl;
-
 	}
 
-
+	////DEMO
 	//for (int i = 0; i < 256; i++) {
 	//    std::cout << toBin(i) << ((i == toDec(toBin(i))) ? "==" : "!=") << toDec(toBin(i)) << std::endl;
 	//}
